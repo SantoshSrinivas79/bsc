1189ee038dda1bca55e6f3e711bdc46c1795d1e7
frappe <frappe@ubuntu.vm>
2020-06-21 21:41:30 +0000

    After add check duplicate in bsc_initiative_assignement

--- bsc/bsc/doctype/bsc_initiative_assignment/bsc_initiative_assignment.py
+++ bsc/bsc/doctype/bsc_initiative_assignment/bsc_initiative_assignment.py
@@ -1,10 +1,23 @@
-# -*- coding: utf-8 -*-
+	 # -*- coding: utf-8 -*-
 # Copyright (c) 2020, Ahmed Mohammed Alkuhlani and contributors
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
+from frappe import _, throw
 from frappe.model.document import Document
 
 class BSCInitiativeAssignment(Document):
-	pass
+	def validate(self):
+		self.validate_duplicate()
+
+	def validate_duplicate(self):
+		conditions = ""
+		conditions += " where docstatus < 2 and department = '%s'" % self.department
+		conditions += " and bsc_indicator = '%s'" % self.bsc_indicator
+		conditions += " and fiscal_year= '%s'" % self.fiscal_year
+		if frappe.db.exists(self.doctype, self.name):
+			conditions += " and name <> '%s'" % self.name
+		sum_name = frappe.db.sql("""select count(name) from `tabBSC Initiative Assignment` %s"""% conditions)[0][0]
+		if sum_name > 0:
+			frappe.throw(_("Already exists with same Department and Indicator and Fiscal Year"))
