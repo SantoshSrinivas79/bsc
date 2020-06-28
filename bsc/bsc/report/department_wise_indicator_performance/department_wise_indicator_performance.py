a09a851b8094b33fb8b60e096ba7bf808a783832
frappe <frappe@ubuntu.vm>
2020-06-28 09:33:25 +0000

    Inhancment

--- bsc/bsc/report/department_wise_indicator_performance/department_wise_indicator_performance.py
+++ bsc/bsc/report/department_wise_indicator_performance/department_wise_indicator_performance.py
@@ -93,8 +93,9 @@ def get_tar(ind,dep,year,total,filters):
 	tar = frappe.db.sql("""SELECT IFNULL(sum(entry_number),0.0) FROM `tabBSC Ledger Entry` WHERE party_type='BSC Target' and entry_type='Targeted' %s""" % conditions,filters)
 	ach = frappe.db.sql("""SELECT IFNULL(sum(entry_number),0.0) FROM `tabBSC Ledger Entry` WHERE party_type='BSC Target' and entry_type='Achieved' %s""" % conditions,filters)
 	per = 0.0
-	if flt(tar[0][0])>0.0:
-		per = flt(((flt(ach[0][0])/flt(tar[0][0]))*100),2)
+	if tar:
+		if flt(tar[0][0])>0.0:
+			per = flt(((flt(ach[0][0])/flt(tar[0][0]))*100),2)
 	return tar[0][0] if tar[0][0] else 0, ach[0][0] if ach[0][0] else 0, per if per else 0
 
 
